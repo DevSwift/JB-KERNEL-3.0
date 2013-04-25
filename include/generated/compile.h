95c6949b0157561dff034df40ef889774d8fd7d9
DevSwift <pawel123485@gmail.com>
2013-04-25 21:49:10 +0200

    Support for Kumquat and Pepper

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Apr 25 20:52:19 CEST 2013"
+#define LINUX_COMPILE_BY "dev"
+#define LINUX_COMPILE_HOST "dev-R520-R522-R620"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
